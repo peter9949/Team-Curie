5cdb7be203816bffbc799f816929fbe7ce88a02c
Xuanyu Fang <47069547+fangx52@users.noreply.github.com>
2021-08-02 11:10:51 -0500

    Create Sophie.java

---
+++ Sophie.java
@@ -0,0 +1,14 @@
+// File Information: Java file introducing name, email, slack and biostack
+// To run the program on terminal: 
+// javac Sophie.java
+// java Sophie
+
+class Sophie {
+    public static void main(String[] args) {
+        String name = "Sophie Fang\n";
+        String email = "fangx@bu.edu\n";
+        String slack = "@Sophie\n";
+        String biostack = "Software Development\n";
+        System.out.println("Name: " + name + "Email: " + email + "Slack: " + slack + "Biostack: " + biostack);
+    }
+}
