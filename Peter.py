dd4a9b8a9b3500749540b6c36c78e63b3b688cba
peter9949 <83517287+peter9949@users.noreply.github.com>
2021-08-02 19:39:35 +0200

    Update Peter.py

--- Peter.py
+++ Peter.py
@@ -1,4 +1,4 @@
-# Script that prints my name, email, slack username and biostack
+#Script that prints my name, email, slack username and biostack
 name = "Peter"
 email: str = "fassankpeter@gmail.com"
 slack_username: str = "@Peter"
