952294029bbae1016203400f373e443f35f430fc
beyu88 <88160848+beyu88@users.noreply.github.com>
2021-08-02 14:55:26 +0530

    This is the R script containing my details for the HackBio internship.

---
+++ Matthew.R
@@ -0,0 +1,8 @@
+print(c("name = Matthew Efoli", "email = matthewefoli@gmail.com", "slack username = @Matthew", "biostack = Data Science"))
+
+#Alternatively
+
+print("name = Matthew Efoli")
+print("email = matthewefoli@gmail.com")
+print("slack = @Matthew")
+print("biostack = Data Science")
