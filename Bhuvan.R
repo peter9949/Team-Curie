e2ca5eaeebcbdec7422381518e05adfeb01d6d27
beyu88 <88160848+beyu88@users.noreply.github.com>
2021-08-02 20:50:34 +0530

    Update Bhuvan.R

--- Bhuvan.R
+++ Bhuvan.R
@@ -3,3 +3,7 @@ email = "bhuvanuthaiah.18bt@kct.ac.in"
 slack = "@Bhuvan"
 biostack = "genomics"
 print(c(Name=name,Email=email,Slack=slack,Biostack=biostack))
+
+#Alternatively
+
+print(c(Name="Bhuvan",Email="bhuvanuthaiah.18bt@kct.ac.in",Slack="@Bhuvan",Biostack="genomics"))
