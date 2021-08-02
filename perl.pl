4320ec60ef68efb095d6806b07b598f89ad9f063
AnanyaSaji <88286303+AnanyaSaji@users.noreply.github.com>
2021-08-02 22:43:10 +0530

    Add files via upload

---
+++ perl.pl
@@ -0,0 +1,14 @@
+#!/usr/bin/perl
+use strict;
+
+my $Name= "Ananya Saji\n";
+print $Name;
+
+my $Email= "ananyasaji501\@gmail.com\n";
+print $Email;
+
+my $Display_Name= "\@Ananya\n";
+print $Display_Name;
+
+my $Biostack= "Genomics\n";
+print $Biostack;
