0cd0bbe3d0a45304cd3574dcd65dc522656147ef
peter9949 <83517287+peter9949@users.noreply.github.com>
2021-08-02 19:35:32 +0200

    Add files via upload
    Update Peter

---
+++ Peter.py
@@ -0,0 +1,6 @@
+# Script that prints my name, email, slack username and biostack
+name = "Peter"
+email: str = "fassankpeter@gmail.com"
+slack_username: str = "@Peter"
+biostack = "transcriptomics"
+print(name, email, slack_username, biostack)
