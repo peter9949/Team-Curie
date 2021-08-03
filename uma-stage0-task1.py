b67fd5121ecff8fdb3f475e34b51014f4caf1976
Umasri Sankarlal <77769613+umasrisankarlal@users.noreply.github.com>
2021-08-03 20:54:49 +0530

    Delete uma-stage0-task1.py

--- uma-stage0-task1.py
+++
@@ -1,7 +0,0 @@
-def member_details():
-    name = "Umasri Sankarlal"
-    email = "umasrisankarlal@gmail.com"
-    slackname = "@UmasriSankarlal"
-    biostack = "Genomics"
-    print(name, email, slackname, biostack, sep=", ")
-member_details()
