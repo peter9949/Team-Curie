b66da1f6f2adc71eaaee81ea9e4e343182165873
Umasri Sankarlal <77769613+umasrisankarlal@users.noreply.github.com>
2021-08-03 20:55:25 +0530

    Create script_umasri.py

---
+++ script_umasri.py
@@ -0,0 +1,7 @@
+def member_details():
+    name = "Umasri Sankarlal"
+    email = "umasrisankarlal@gmail.com"
+    slackname = "@UmasriSankarlal"
+    biostack = "Genomics"
+    print(name, email, slackname, biostack, sep=", ")
+member_details()
