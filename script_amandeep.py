b7d2f1771a3e27c274f48ef1ebc2ec2244e96471
662amandeep <88246395+662amandeep@users.noreply.github.com>
2021-08-03 19:57:14 +0530

    Add files via upload

---
+++ script_amandeep.py
@@ -0,0 +1,10 @@
+Python 3.9.5 (tags/v3.9.5:0a7dcbd, May  3 2021, 17:27:52) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> #Script that prints my name, email, slack username and biostack
+>>> name = "Amandeep Kaur"
+>>> email: str = "662amandeep@gmail.com"
+>>> slack_username: str = "@Amandeep"
+>>> biostack = "Genomics"
+>>> print ("{0}, {1}, {2}, {3}".format(name, email, slack_username, biostack))
+Amandeep Kaur, 662amandeep@gmail.com, @Amandeep, Genomics
+>>> 
