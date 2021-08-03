21a5117c468be1d7c4f3b64c73e04e1a344cd1d2
sooraj14072001 <88287926+sooraj14072001@users.noreply.github.com>
2021-08-03 20:06:19 +0530

    script_sooraj.java

---
+++ script_sooraj.java
@@ -0,0 +1,11 @@
+public class Sooraj
+{
+   public static void main(String[] args)
+   {
+   String name = "Sooraj S";
+   String email = "sooraj 14072001@gmail.com";
+   String slack= "@Sooraj";
+   String biostack = "Genomics";       
+   System.out.println(name + ", "+ email + ", " + slack ", " + biostack);
+    }
+}
