514d5b7b92c0b27cbbc03e72b1448823e76b728b
Ananya <88286303+AnanyaSaji@users.noreply.github.com>
2021-08-03 09:30:29 +0530

    Update Ananya.pl

--- Ananya.pl
+++ Ananya.pl
@@ -1,14 +1,6 @@
 #!/usr/bin/perl
 use strict;
+use warnings;
 
-my $Name= "Ananya Saji\n";
-print $Name;
-
-my $Email= "ananyasaji501\@gmail.com\n";
-print $Email;
-
-my $Display_Name= "\@Ananya\n";
-print $Display_Name;
-
-my $Biostack= "Genomics\n";
-print $Biostack;
+my @A= ("Ananya Saji,", " ananyasaji\@gmail.com,"," \@Ananya,", " Genomics");
+print @A;
