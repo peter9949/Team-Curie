7c5f0f4b52f28b76b55612905694a4af3580cc53
TosinAkinola <88287437+TosinAkinola@users.noreply.github.com>
2021-08-03 07:43:09 -0700

    Update Scipt_Tosin.py

--- Scipt_Tosin.py
+++ Scipt_Tosin.py
@@ -1,6 +1,6 @@
-@@ -1,6 +1,6 @@
+@@ -0,0 +1,5 @@
 name: "Tosin Akinola"
 email: str = "tosin.akinola.at@gmail.com"
 slack_username: str = "@Tosin"
 biostack = "drug development"
-+print("{0}, {1}, {2}, {3}" .format(name, email, slack_username, biostack))
+print("{0}, {1}, {2}, {3}" .format(name, email, slack_username, biostack))
