bdb04defcb010f4dbef6b620053b8635ce7f84ab
maansisrivastava <62357104+maansisrivastava@users.noreply.github.com>
2021-08-03 23:15:10 +0530

    Delete script_Maansi.py

--- script_Maansi.py
+++
@@ -1,5 +0,0 @@
-name = "Maansi Srivastava"
-email_id = "srivastava.maansi@gmail.com"
-slack_username = "@Maansi"
-biostack = "Transcriptomics"
-print("{} {} {} {}".format(name, email_id, slack_username, biostack))
