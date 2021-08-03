369587c92e2427b691a4ce1887b4a9f543a45577
beyu88 <88160848+beyu88@users.noreply.github.com>
2021-08-03 14:30:10 +0530

    Update Bhuvan.R

--- Bhuvan.R
+++ Bhuvan.R
@@ -1,9 +1,1 @@
-name = "Bhuvan"
-email = "bhuvanuthaiah.18bt@kct.ac.in"
-slack = "@Bhuvan"
-biostack = "genomics"
-print(c(Name=name,Email=email,Slack=slack,Biostack=biostack))
-
-#Alternatively
-
-print(c(Name="Bhuvan",Email="bhuvanuthaiah.18bt@kct.ac.in",Slack="@Bhuvan",Biostack="genomics"))
+print(c("Bhuvan Uthaiah Muruvanda","bhuvanuthaiah.18bt@kct.ac.in","@Bhuvan","genomics"), quote = FALSE)
