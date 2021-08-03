5bb7be9041151f3bd2faf32b8d96c901db5ce872
tasneem786 <60441452+tasneem786@users.noreply.github.com>
2021-08-03 14:27:55 +0530

    Stage0_Tasneem

---
+++ Tasneem.py
@@ -0,0 +1,6 @@
+Name = "Tasneem Juzer"
+Email = "tasneem721@gmail.com"
+Slack_username = "@Tasneem"
+Biostack = "Genomics"
+intern_info = Name + " , " + Email + " , " + Slack_username + " , " + Biostack 
+print ( intern_info )
