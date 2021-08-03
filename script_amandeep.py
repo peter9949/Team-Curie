8260c5099d61e0c88e7f09df789f2afd6b9c0427
662amandeep <88246395+662amandeep@users.noreply.github.com>
2021-08-03 23:47:53 +0530

    Delete script_amandeep.py

--- script_amandeep.py
+++
@@ -1,6 +0,0 @@
-#!/usr/local/bin/python3
-name = "Amandeep Kaur"
-email: str = "662amandeep@gmail.com"
-slack_username: str = "@Amandeep"
-biostack = "Genomics"
-print ("{0}, {1}, {2}, {3}".format(name, email, slack_username, biostack))
