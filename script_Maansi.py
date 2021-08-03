9234a6a4863ade2f435eede20ea9e830635a9dc2
maansisrivastava <62357104+maansisrivastava@users.noreply.github.com>
2021-08-03 23:14:25 +0530

    Update script_Maansi.py

--- script_Maansi.py
+++ script_Maansi.py
@@ -2,4 +2,4 @@ name = "Maansi Srivastava"
 email_id = "srivastava.maansi@gmail.com"
 slack_username = "@Maansi"
 biostack = "Transcriptomics"
-print("{} {} {} {}".format(name, email_id, slack_username, biostack)
+print("{} {} {} {}".format(name, email_id, slack_username, biostack))
