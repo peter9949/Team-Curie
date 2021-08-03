0763bf692af284ba32bd17c70498a7a351530be5
askkarteek <88286218+askkarteek@users.noreply.github.com>
2021-08-03 20:37:32 +0530

    Create script_karteek.py

---
+++ script_karteek.py
@@ -0,0 +1,5 @@
+name = "Karteek Akshinthala,"
+email = "askkarteek@gmail.com,"
+slack_id = "@karteekakshinthala,"
+biostack = "Genomics"
+print("{} {} {} {}".format(name, email, slack_id, biostack))
