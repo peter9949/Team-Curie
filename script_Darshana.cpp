2ce44c6a0a409fdc4c7469db999938963629c38c
darshana2509 <83279269+darshana2509@users.noreply.github.com>
2021-08-04 20:48:19 +0530

    Update script_Darshana.cpp

--- script_Darshana.cpp
+++ script_Darshana.cpp
@@ -1,12 +1,24 @@
 #include<iostream>
 
 using namespace std;
+int hammingDist(string slack, string twitter)
+{
+	int i = 0, count = 0;
+	while (slack[i] != '\0')
+	{
+		if (slack[i] != twitter[i])
+			count++;
+		i++;
+	}
+	return count;
+}
 int main()
 {
 	string name = "Darshana Joshi";
 	string email = "darshanajoshi762@gmail.com";
 	string slack = "@DarshanaJoshi";
 	string biostack = "Transcriptomics";
-	cout<<name<<", "<<email<<", "<<slack<<", "<<biostack;
+	string twitter = "@darshana25999";
+	cout<<name<<", "<<email<<", "<<slack<<", "<<biostack<<", "<<twitter<<", "<<hammingDist(slack, twitter);
 	return 0;
 }
