2e38f9fb98843cd889eba996374d3b8313a8fced
ayushpanda2002 <88286571+ayushpanda2002@users.noreply.github.com>
2021-08-04 19:55:51 +0000

    Update script_Maansi.py

--- script_Maansi.py
+++ script_Maansi.py
@@ -4,4 +4,15 @@ email_id = "srivastava.maansi@gmail.com"
 slack_username = "@Maansi"
 biostack = "Transcriptomics"
 twitter = "@maansi"
-print("{} {} {} {} {}".format(name, email_id, slack_username, biostack, twitter))
+ def hammingdist (slack_username, twitter) :
+	i = 0
+	count =0
+	while (i < len (slack_username)):
+		if (slack_username [i] != twitter [i]) :
+			count +=1
+		i +=1
+	return count
+
+hamming_distance = (hammingdist(slack_username, twitter))
+print("{}, {}, {}, {}, {}, {}".format(name, email, slack_username, biostack, twitter, hamming_distance))
+
