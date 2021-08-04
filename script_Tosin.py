01c16558c7e9d1090abb67e4ad2277afe58fd501
TosinAkinola <88287437+TosinAkinola@users.noreply.github.com>
2021-08-04 08:44:31 -0700

    Update script_Tosin.py

--- script_Tosin.py
+++ script_Tosin.py
@@ -1,6 +1,22 @@
 #!/usr/local/bin/python3
 name: str = "Tosin Akinola"
 email: str = "tosin.akinola.at@gmail.com"
-slack_username: str = "@Tosin"
+slack_username: str = "@TosinAkinola"
 biostack = "drug development"
-print("{0}, {1}, {2}, {3}" .format(name, email, slack_username, biostack))
+twitter_handle = "AkinolaTosinS"
+def hamming_distance(str1, str2):
+  i = 0
+  count = 0
+  
+  while(i < lens(str1)):
+    if(str1[i] != str2[1]):
+      count += 1
+      i +=1
+      return count
+    
+    # Driver code
+    str1 = "TosinAkinola"
+    str2 = "AkinolaTosinS"
+    
+    #function call
+print(name, email, slack_username, biostack,twitter_handle, hamming_distance(str1, str2), sep=", ")
