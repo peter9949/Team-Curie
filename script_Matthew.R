db9ebbf24448539c2ae7a15e60cbc018ad6a8fa0
Matthew <matthewefoli@gmail.com>
2021-08-04 05:39:44 -0700

    Updates

--- script_Matthew.R
+++ script_Matthew.R
@@ -1,4 +1,11 @@
 #!/usr/local/bin/Rscript
 
-my_details <-c("Matthew Efoli","matthewefoli@gmail.com","Data Science","@MatthewEfoli")
+my_details <-c("Matthew Efoli","matthewefoli@gmail.com","Data Science","@MatthewEfoli1", "@MatthewEfosa1")
 cat(paste(my_details, collapse = ','))
+
+# Calculating the Hamming distance between my twitter and slack user handles
+
+twitter <- "@MatthewEfosa1"
+slack <- "@MatthewEfoli1"
+
+sum(twitter != slack) #hamming distance
