0096c4d3787d28241b7fb3253f38e579667107c4
askkarteek <88286218+askkarteek@users.noreply.github.com>
2021-08-04 20:29:46 +0530

    Update script_karteek.py

--- script_karteek.py
+++ script_karteek.py
@@ -3,4 +3,20 @@ name = "Karteek Akshinthala"
 email = "askkarteek@gmail.com"
 slack_id = "@karteekakshinthala"
 biostack = "Genomics"
-print(name, email, slack_id, biostack, sep=",")
+twitter = "teeeek7"
+def hammingDist(str1, str2):
+    i = 0
+    count = 0
+ 
+    while(i < len(str1)):
+        if(str1[i] != str2[i]):
+            count += 1
+        i += 1
+    return count
+ 
+# Driver code 
+str1 = "Karteek"
+str2 = "teeeek7"
+ 
+# function call
+print(name, email, slack_id, biostack, twitter, hammingDist(str1, str2), sep=", ")
