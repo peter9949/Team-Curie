f5edde3205512c7ba214b4d31b85430c65f3ee08
Umasri Sankarlal <77769613+umasrisankarlal@users.noreply.github.com>
2021-08-04 15:17:36 +0530

    Update script_umasri.py

--- script_umasri.py
+++ script_umasri.py
@@ -4,6 +4,6 @@ def member_details():
     email = "umasrisankarlal@gmail.com"
     slackname = "@UmasriSankarlal"
     biostack = "Genomics"
-    twittername = "@Umasri52470207"
+    twittername = "@Umasri524702071"
     print(name, email, slackname, biostack, twittername, sep=", ")
 member_details()
