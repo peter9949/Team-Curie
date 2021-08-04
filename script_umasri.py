4a23ae082c272eea5799d0dbc04b077e8e9f927a
Umasri Sankarlal <77769613+umasrisankarlal@users.noreply.github.com>
2021-08-04 16:25:07 +0530

    Update script_umasri.py

--- script_umasri.py
+++ script_umasri.py
@@ -7,3 +7,21 @@ def member_details():
     twittername = "@Umasri524702071"
     print(name, email, slackname, biostack, twittername, sep=", ")
 member_details()
+
+#script with hamming distance calculation
+#!/usr/local/bin/python3
+name = "Umasri Sankarlal"
+email = "umasrisankarlal@gmail.com"
+slackname = "@UmasriSankarlal"
+biostack = "Genomics"
+twittername = "@Umasri524702071"
+def hammingDist(slackname, twittername):
+    i = 0
+    count = 0
+    while(i < len(slackname)):
+        if(slackname[i] != twittername[i]):
+            count += 1
+        i += 1
+    return count
+hummingdistance = (hammingDist(slackname, twittername))
+print (name, email, slackname, biostack, twittername, hummingdistance, sep=", ")
