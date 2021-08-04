9d0ecd797df9745cc74b5b45cbcb13648fc90a11
peter9949 <83517287+peter9949@users.noreply.github.com>
2021-08-04 13:41:55 +0200

    Update script_peter.py

--- script_peter.py
+++ script_peter.py
@@ -2,5 +2,23 @@
 name = "Peter Fassan"
 email: str = "fassankpeter@gmail.com"
 slack_username: str = "@Peter"
-biostack = "transcriptomics"
+biostack = "Transcriptomics"
 print("{0}, {1}, {2}, {3}".format(name, email, slack_username, biostack))
+
+#Script that calculates hamming distance between slack username and twitter username
+name = "Peter Fassan"
+email: str = "fassankpeter@gmail.com"
+slack_username: str = "@Peter"
+biostack = "Transcriptomics"
+twitter_id = "@Fkp99"
+
+def hammingDist(slack_username, twitter_id):
+    f = 0
+    count = 0
+    while(f< len(slack_username)):
+        if(slack_username[f] != twitter_id[f]):
+            count += 1
+        f += 1
+    return count
+hamming_distance = (hammingDist(slack_username, twitter_id))
+print (name, email, slack_username, biostack, twitter_id, hamming_distance, sep=", ")
