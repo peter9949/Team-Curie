78caacdb005f0e7ce9583317f62eea83020340b5
askkarteek <88286218+askkarteek@users.noreply.github.com>
2021-08-04 12:09:19 +0530

    Update script_karteek.py

--- script_karteek.py
+++ script_karteek.py
@@ -3,4 +3,4 @@ name = "Karteek Akshinthala"
 email = "askkarteek@gmail.com"
 slack_id = "@karteekakshinthala"
 biostack = "Genomics"
-print("{} {} {} {}".format(name, email, slack_id, biostack))
+print(name, email, slack_id, biostack, sep=",")
