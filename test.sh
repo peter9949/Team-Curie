0d81c357734d3feb5f0f0502e24317d0ef8ace49
Xuanyu Fang <47069547+fangx52@users.noreply.github.com>
2021-08-04 12:10:48 -0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -4,29 +4,44 @@ NAME=$(echo $REPO|sed -r 's^./(.).git^\1^')
 OUTPUT_FILE=Team-Curie.csv
 git clone $REPO
 cd $NAME
+sudo apt-get update
+#Download R
+sudo apt -y install r-base
+#Download node
+sudo apt install nodejs
+#Download python
+sudo apt install python3.6
+#Download perl
+sudo apt install perl
+#Download c++
+sudo apt install g++
+#Download c
+sudo apt install gcc
+#Download java
+sudo apt install openjdk-8-jre
 for file in $(ls);
 do
         if [[ $file == *.py ]]; 
 	      then
-                python $file |sed 's/ /,/g' >> $OUTPUT_FILE
+                python $file >> $OUTPUT_FILE
       	elif [[ $file == *.js ]];
 	      then
-                node $file |sed 's/ /,/g' >> $OUTPUT_FILE
+                node $file >> $OUTPUT_FILE
         elif [[$file == *.cpp]];
         then
-                c++ $file |sed 's/ /,/g' >> $OUTPUT_FILE
+                c++ $file>> $OUTPUT_FILE
         elif [[$file == *.pl]];
         then
-                Perl $file |sed 's/ /,/g' >> $OUTPUT_FILE
+                Perl $file>> $OUTPUT_FILE
         elif [[$file == *.c]];
         then
-                c $file |sed 's/ /,/g' >> $OUTPUT_FILE
+                c $file >> $OUTPUT_FILE
         elif [[$file == *.java]];
         then
-                java $file |sed 's/ /,/g' >> $OUTPUT_FILE
+                java $file>> $OUTPUT_FILE
           
         elif [[ $file == *.R ]];
 	      then
-                Rscript $file |sed G |sed 's/ /,/g' >> $OUTPUT_FILE  
+                Rscript $file>> $OUTPUT_FILE  
 	fi
 done
