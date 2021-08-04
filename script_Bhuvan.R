d8320d93ac9cb51ba702408f46e6c0870bc3cff5
beyu88 <88160848+beyu88@users.noreply.github.com>
2021-08-04 14:11:25 +0530

    latest changes added

--- script_Bhuvan.R
+++ script_Bhuvan.R
@@ -1,4 +1,4 @@
 #!/usr/local/bin/Rscript
 
-x <- c("Bhuvan Uthaiah Muruvanda","bhuvanuthaiah.18bt@kct.ac.in","@Bhuvan","genomics")
+x <- c("Bhuvan Uthaiah Muruvanda","bhuvanuthaiah.18bt@kct.ac.in","@Bhuvan","genomics","@Bhuv88","2")
 cat(paste(x, collapse = ","))
