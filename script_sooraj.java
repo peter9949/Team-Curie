e9cab653ba53897e87e01085c28f36998e5fa2c6
sooraj14072001 <88287926+sooraj14072001@users.noreply.github.com>
2021-08-04 21:15:24 +0530

    script_sooraj.java

--- script_sooraj.java
+++ script_sooraj.java
@@ -1,11 +1,25 @@
 public class Sooraj
-{
-   public static void main(String[] args)
+ {
+  static int hammingDist(String str1, String str2)
+  {
+   int i = 0, count = 0;
+   while (i < str1.length())
    {
-   String name = "Sooraj S";
-   String email = "sooraj 14072001@gmail.com";
-   String slack= "@Sooraj";
-   String biostack = "Genomics";       
-   System.out.println(name + ", "+ email + ", " + slack ", " + biostack);
-    }
+    if (str1.charAt(i) != str2.charAt(i))
+    count++;
+    i++;
+   }
+   return count;
+  }
+public static void main (String[] args)
+{
+String name = "Sooraj S";
+String email = "sooraj14072001@gmail.com";
+String slack= "@Sooraj";
+String twitter= "@SooRAJ";
+String biostack = "Genomics";
+String str1 = "@Sooraj";
+String str2 = "@SooRAJ";
+ System.out.println(name + ", " + email + ", " + slack +", " + biostack + ", " + twitter + ", " + hammingDist (str1, str2));
+}
 }
