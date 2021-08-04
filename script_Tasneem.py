0815ae57b631d4f1c412d935f967f0fbfa5c56fe
tasneem786 <60441452+tasneem786@users.noreply.github.com>
2021-08-04 21:25:10 +0530

    Update script_Tasneem.py

--- script_Tasneem.py
+++ script_Tasneem.py
@@ -3,5 +3,15 @@ Name = "Tasneem Juzer"
 Email = "tasneem721@gmail.com"
 Slack_username = "@Tasneem"
 Biostack = "Genomics"
-intern_info = Name + " , " + Email + " , " + Slack_username + " , " + Biostack 
+Twitter_id = "@Tassu22"
+def hammingdist(Slack_username, Twitter_id ):
+    i = 0
+    count = 0
+    while(i < len(Slack_username)):
+        if(Slack_username[i] != Twitter_id [i]):
+            count += 1
+        i += 1
+    return count
+ 
+intern_info = Name + " , " + Email + " , " + Slack_username + " , " + Biostack + " , " + Twitter_id + " , " + str(hammingdist(Slack_username, Twitter_id ))
 print ( intern_info )
