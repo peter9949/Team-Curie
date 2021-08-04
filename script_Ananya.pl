0330cb22fbec6ffe931bc54235fa319c990d885a
Ananya <88286303+AnanyaSaji@users.noreply.github.com>
2021-08-04 23:50:25 +0530

    Update script_Ananya.pl

--- script_Ananya.pl
+++ script_Ananya.pl
@@ -2,5 +2,14 @@
 use strict;
 use warnings;
 
-my @A= ("Ananya Saji,", " ananyasaji501\@gmail.com,"," \@Ananya,", " Genomics");
-print @A;
+my $name ="Ananya Saji,";
+my $email =" ananyasaji501\@gmail.com,";
+my $slack_username =" \@Ananya,";
+my $biostack =" Genomics,";
+my $twitter_id =" \@12Anu_,";
+my $s1 =" Ananya";
+my $s2 =" 12Anu_";
+
+print ($name,$email, $slack_username, $biostack, $twitter_id);
+print " ", hd( $s1, $s2 );
+sub hd{ length( $_[ 0 ] ) - ( ( $_[ 0 ] ^ $_[ 1 ] ) =~ tr[\0][\0] ) }
