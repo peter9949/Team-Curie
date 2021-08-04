3767fe62cb909b92fded1df4de98a42f0ffc575c
Ananya <88286303+AnanyaSaji@users.noreply.github.com>
2021-08-04 23:56:58 +0530

    Update script_Ananya.pl

--- script_Ananya.pl
+++ script_Ananya.pl
@@ -10,6 +10,6 @@ my $twitter_id =" \@12Anu_,";
 my $s1 =" Ananya";
 my $s2 =" 12Anu_";
 
-print ($name,$email, $slack_username, $biostack, $twitter_id);
+print ($name,$email,$slack_username,$biostack,$twitter_id);
 print " ", hd( $s1, $s2 );
 sub hd{ length( $_[ 0 ] ) - ( ( $_[ 0 ] ^ $_[ 1 ] ) =~ tr[\0][\0] ) }
