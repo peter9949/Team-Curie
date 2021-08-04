c4f8d9a754e0844cd9ea80c044b0fecbfec26933
Xuanyu Fang <47069547+fangx52@users.noreply.github.com>
2021-08-04 13:48:59 -0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,46 +1,32 @@
 #!/bin/bash
 REPO=https://github.com/peter9949/Team-Curie.git
-NAME=$(echo $REPO|sed -r 's^./(.).git^\1^')
+# NAME=$(echo $REPO|sed -r 's^./(.).git^\1^')
 OUTPUT_FILE=Team-Curie.csv
 git clone $REPO
-cd $NAME
-sudo apt-get update
-#Download R
-sudo apt-get -y install r-base
-#Download node
-sudo apt-get install nodejs
-#Download python
-sudo apt-get install python3.6
-#Download perl
-sudo apt-get install perl
-#Download c++
-sudo apt-get install g++
-#Download c
-sudo apt-get install gcc
-#Download java
-sudo apt-get install openjdk-8-jre
+cd ./Team-Curie
+
 for file in $(ls);
 do
         if [[ $file == *.py ]]; 
 	      then
-                python $file >> $OUTPUT_FILE
+                python3 $file >> $OUTPUT_FILE
       	elif [[ $file == *.js ]];
 	      then
                 node $file >> $OUTPUT_FILE
-        elif [[$file == *.cpp]];
-        then
-                g++ $file>> $OUTPUT_FILE
-        elif [[$file == *.pl]];
-        then
-                perl $file>> $OUTPUT_FILE
-        elif [[$file == *.c]];
-        then
-                gcc $file >> $OUTPUT_FILE
-        elif [[$file == *.java]];
-        then
-                javac $file
-                name=`echo $file | cut -d\. -f1`
-                java $name>> $OUTPUT_FILE
+        elif [[ $file == *.cpp ]];
+              then
+                 g++ $file>> $OUTPUT_FILE
+        elif [[ $file == *.pl ]];
+              then
+                 perl $file>> $OUTPUT_FILE
+        elif [[ $file == *.c ]];
+              then
+                 gcc $file >> $OUTPUT_FILE
+        elif [[ $file == *.java ]];
+              then
+                 javac $file
+                 name=`echo $file | cut -d\. -f1`
+                 java $name>> $OUTPUT_FILE
           
         elif [[ $file == *.R ]];
 	      then
