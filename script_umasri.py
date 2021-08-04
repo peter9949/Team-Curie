ea505137795babb636176322b77362a75d438c91
ayushpanda2002 <88286571+ayushpanda2002@users.noreply.github.com>
2021-08-04 19:30:51 +0000

    Update script_umasri.py

--- script_umasri.py
+++ script_umasri.py
@@ -5,7 +5,7 @@ def member_details():
     slackname = "@UmasriSankarlal"
     biostack = "Genomics"
     twittername = "@Umasri524702071"
-    print(name, email, slackname, biostack, twittername, sep=", ")
+   # print(name, email, slackname, biostack, twittername, sep=", ")
 member_details()
 
 #script with hamming distance calculation
