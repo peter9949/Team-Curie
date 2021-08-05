c7223c31f09df5ad5c8d70aadae46184c85eff15
ayushpanda2002 <88286571+ayushpanda2002@users.noreply.github.com>
2021-08-05 11:20:25 +0530

    Update script_ayush.c

--- script_ayush.c
+++ script_ayush.c
@@ -1,6 +1,27 @@
 #include<stdio.h>
-int main()
+#include<string.h>
+
+int hammingdist(char slack_username[ ], char twitter[ ])
 {
-    printf("Ayush Panda, ayush.panda@niser.ac.in, @Ayush, Drug Development");
+  int i=0, count=0, l;
+  l = strlen(slack_username);
+  for (i=0; i < l; i++)
+  {
+    if(slack_username[i] != twitter[i])
+    {
+      count+=1;
+    }
+  }
+ return count;
+}
+
+
+int main()
+
+{
+
+    int hamming_dist = hammingdist("@Ayush", "@Ayu02");
+    printf("Ayush Panda, ayush.panda@niser.ac.in, Drug Development, @Ayush, @Ayu02, %d", hamming_dist);
+
     return 0;
 }
