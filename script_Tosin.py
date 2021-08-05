a91ea2c9680a5dd799703cebb1d5a61409c6533f
ayushpanda2002 <88286571+ayushpanda2002@users.noreply.github.com>
2021-08-05 07:00:35 +0000

    Update script_Tosin.py

--- script_Tosin.py
+++ script_Tosin.py
@@ -3,7 +3,7 @@ name = "Tosin Akinola"
 email = "tosin.akinola.at@gmail.com"
 slack_username = "@TosinAkinola"
 biostack = "drug development"
-twitter_handle = "AkinolaTosinS"
+twitter_handle = "@AkinolaTosin"
 def hamming_distance (slack_username, twitter_handle):
   i = 0
   count = 0
